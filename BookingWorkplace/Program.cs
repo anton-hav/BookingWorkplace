aace6b6bb838838bbfc68e9674613c038558716c
anton-hav <anton.havrilenia@hotmail.com>
2022-11-18 14:44:52 +0100

    add Serilog for logging

--- BookingWorkplace/Program.cs
+++ BookingWorkplace/Program.cs
@@ -1,3 +1,8 @@
+using Serilog;
+using Serilog.Events;
+using System.Reflection;
+using System.Text;
+
 namespace BookingWorkplace
 {
     public class Program
@@ -5,6 +10,11 @@ namespace BookingWorkplace
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Host.UseSerilog((ctx, lc) => lc
+                .WriteTo.Console()
+                .WriteTo.File(GetPathToLogFile(),
+                    LogEventLevel.Information));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
@@ -31,6 +41,20 @@ namespace BookingWorkplace
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
+        }
+
+        /// <summary>
+        ///     Returns the path for log file recording.
+        /// </summary>
+        /// <returns>A string whose value contains a path to the log file</returns>
+        private static string GetPathToLogFile()
+        {
+            var sb = new StringBuilder();
+            sb.Append(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            sb.Append(@"\logs\");
+            sb.Append($"{DateTime.Now:yyyyMMddhhmmss}");
+            sb.Append("data.log");
+            return sb.ToString();
         }
     }
 }
