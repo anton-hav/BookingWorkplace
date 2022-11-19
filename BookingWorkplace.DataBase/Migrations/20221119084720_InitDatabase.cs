e67b9db8ec0e872e3ba9b09462f4e969fa69a1f7
anton-hav <anton.havrilenia@hotmail.com>
2022-11-19 17:11:05 +0100

    add init migration

---
+++ BookingWorkplace.DataBase/Migrations/20221119084720_InitDatabase.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingWorkplace.DataBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Equipments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equipments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Number = table.Column<int>(type: "int", nullable: false),
+                    Floor = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Workplaces",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RoomId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DeskNumber = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Workplaces", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Workplaces_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EquipmentForWorkplaces",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    EquipmentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    WorkplacesId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    WorkplaceId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Count = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EquipmentForWorkplaces", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EquipmentForWorkplaces_Equipments_EquipmentId",
+                        column: x => x.EquipmentId,
+                        principalTable: "Equipments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EquipmentForWorkplaces_Workplaces_WorkplaceId",
+                        column: x => x.WorkplaceId,
+                        principalTable: "Workplaces",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    EmployeeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    WorkplaceId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TimeFrom = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TimeTo = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reservations_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reservations_Workplaces_WorkplaceId",
+                        column: x => x.WorkplaceId,
+                        principalTable: "Workplaces",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EquipmentForWorkplaces_EquipmentId_WorkplacesId",
+                table: "EquipmentForWorkplaces",
+                columns: new[] { "EquipmentId", "WorkplacesId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EquipmentForWorkplaces_WorkplaceId",
+                table: "EquipmentForWorkplaces",
+                column: "WorkplaceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Equipments_Id",
+                table: "Equipments",
+                column: "Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_EmployeeId_WorkplaceId_TimeFrom_TimeTo",
+                table: "Reservations",
+                columns: new[] { "EmployeeId", "WorkplaceId", "TimeFrom", "TimeTo" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_WorkplaceId",
+                table: "Reservations",
+                column: "WorkplaceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_Number_Floor",
+                table: "Rooms",
+                columns: new[] { "Number", "Floor" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Workplaces_RoomId_DeskNumber",
+                table: "Workplaces",
+                columns: new[] { "RoomId", "DeskNumber" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EquipmentForWorkplaces");
+
+            migrationBuilder.DropTable(
+                name: "Reservations");
+
+            migrationBuilder.DropTable(
+                name: "Equipments");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Workplaces");
+
+            migrationBuilder.DropTable(
+                name: "Rooms");
+        }
+    }
+}
