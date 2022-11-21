f63ba490e1aaad402b5ddd93b2a47ccffe30fbaf
anton-hav <anton.havrilenia@hotmail.com>
2022-11-21 15:13:22 +0100

    add new migration that turn workplace properties from int to string

--- BookingWorkplace.DataBase/Entities/Workplace.cs
+++ BookingWorkplace.DataBase/Entities/Workplace.cs
@@ -3,9 +3,9 @@
 public class Workplace : IBaseEntity
 {
     public Guid Id { get; set; }
-    public int Floor { get; set; }
-    public int Room { get; set; }
-    public int DeskNumber { get; set; }
+    public string Floor { get; set; }
+    public string Room { get; set; }
+    public string DeskNumber { get; set; }
 
     public List<Reservation> Reservations { get; set; }
     public List<EquipmentForWorkplace> EquipmentForWorkplaces { get; set; }
