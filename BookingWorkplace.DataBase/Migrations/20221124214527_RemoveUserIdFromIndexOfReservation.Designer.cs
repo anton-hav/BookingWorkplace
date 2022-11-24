f48eb30fee0bf59d976637ae25eafd7bff57c8b9
anton-hav <anton.havrilenia@hotmail.com>
2022-11-24 22:48:51 +0100

    adds new migration that removes UserId from the index of Reservations

--- BookingWorkplace.DataBase/Migrations/20221124214527_RemoveUserIdFromIndexOfReservation.Designer.cs
+++ BookingWorkplace.DataBase/Migrations/20221124214527_RemoveUserIdFromIndexOfReservation.Designer.cs
@@ -4,6 +4,7 @@ using BookingWorkplace.DataBase;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookingWorkplace.DataBase.Migrations
 {
     [DbContext(typeof(BookingWorkplaceDbContext))]
-    partial class BookingWorkplaceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221124214527_RemoveUserIdFromIndexOfReservation")]
+    partial class RemoveUserIdFromIndexOfReservation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BookingWorkplace.DataBase/Migrations/20221124214527_RemoveUserIdFromIndexOfReservation.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingWorkplace.DataBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUserIdFromIndexOfReservation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Reservations_UserId_WorkplaceId_TimeFrom_TimeTo",
+                table: "Reservations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reservations_WorkplaceId",
+                table: "Reservations");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_UserId",
+                table: "Reservations",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_WorkplaceId_TimeFrom_TimeTo",
+                table: "Reservations",
+                columns: new[] { "WorkplaceId", "TimeFrom", "TimeTo" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Reservations_UserId",
+                table: "Reservations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reservations_WorkplaceId_TimeFrom_TimeTo",
+                table: "Reservations");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_UserId_WorkplaceId_TimeFrom_TimeTo",
+                table: "Reservations",
+                columns: new[] { "UserId", "WorkplaceId", "TimeFrom", "TimeTo" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_WorkplaceId",
+                table: "Reservations",
+                column: "WorkplaceId");
+        }
+    }
+}
